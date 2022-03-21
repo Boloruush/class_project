eb37f9e446a3d8b5fb69c2e31aec35e2165c9152
Boloruush <bolor.a0803@gmail.com>
2022-03-21 10:32:03 +0800

    navigate to second screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:class_project/screens/second_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.deepPurple.shade700,
+        child: Center(
+          child: ElevatedButton(
+            child: Text('Press me'),
+            onPressed: () {
+              print('Hello');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SecondScreen(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
